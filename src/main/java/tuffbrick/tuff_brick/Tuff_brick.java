da636ace34c49e0ea0c2f5c99e586a8f4b4b371c
IgnorbSnapps <csnappyplays@gmail.com>
2022-08-22 07:38:25 -0500

    added like shit n stuff

--- src/main/java/tuffbrick/tuff_brick/Tuff_brick.java
+++ src/main/java/tuffbrick/tuff_brick/Tuff_brick.java
@@ -1,10 +1,38 @@
 package tuffbrick.tuff_brick;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class Tuff_brick implements ModInitializer {
+public static final Block TUFF_BRICK = new Block(FabricBlockSettings.of(Material.STONE).strength(7.0f));
     @Override
     public void onInitialize() {
+        Registry.register(Registry.BLOCK, new Identifier("tuffbricks", "tuff_brick"), TUFF_BRICK);
+        Registry.register(Registry.ITEM, new Identifier("tuffbricks", "tuff_brick"),
+                new BlockItem(TUFF_BRICK, new FabricItemSettings().group(ItemGroup.MISC)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
--- src/main/java/tuffbrick/tuff_brick/client/Tuff_brickClient.java
+++ src/main/java/tuffbrick/tuff_brick/client/Tuff_brickClient.java
@@ -3,11 +3,25 @@ package tuffbrick.tuff_brick.client;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.util.registry.Registry;
 
 @Environment(EnvType.CLIENT)
 public class Tuff_brickClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
