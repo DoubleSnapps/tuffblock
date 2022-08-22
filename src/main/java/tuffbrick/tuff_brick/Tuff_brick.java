228ed75819610724052cbd50574cbfa671a81e61
IgnorbSnapps <csnappyplays@gmail.com>
2022-08-21 20:17:25 -0500

    piis

---
+++ src/main/java/tuffbrick/tuff_brick/Tuff_brick.java
@@ -0,0 +1,10 @@
+package tuffbrick.tuff_brick;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Tuff_brick implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
